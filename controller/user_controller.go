00752d0d29fba2a93703fdd5d1eaa2e7e97f102d
tsadamori <tomoaki.sadamori@gmail.com>
2022-10-12 21:38:13 +0900

    update: add limit and offset options

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -1,11 +1,24 @@
 package controller
 
 import (
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"github.com/tsadamori/kondate-api-golang/model"
 )
 
 func ShowAllUsers(c *gin.Context) {
+	var limit, offset int
+
+	limit, _ = strconv.Atoi(c.Query("limit"))
+	offset, _ = strconv.Atoi(c.Query("offset"))
+	if limit == 0 {
+		limit = 10
+	}
+	if offset == 0 {
+		offset = 0
+	}
+
 	data := model.GetAllUsers()
 	c.JSON(200, data)
 }
